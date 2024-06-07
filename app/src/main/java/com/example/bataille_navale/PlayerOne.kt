34d26d52019cf63c3ec5184babfc1184eeb8eba6
JaxV9 <layanj.pro@gmail.com>
2024-06-07 10:35:48 +0200

    feat: buttons added

--- app/src/main/java/com/example/bataille_navale/PlayerOne.kt
+++ app/src/main/java/com/example/bataille_navale/PlayerOne.kt
@@ -9,6 +9,32 @@ class PlayerOne : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_one)
 
-        findViewById<TextView>(R.id.currentDifficulty).text = intent.extras?.getString("DIFFICULTY")
+        val currentDifficulty = intent.extras?.getString("DIFFICULTY")
+        findViewById<TextView>(R.id.currentDifficulty).text = currentDifficulty
+
+        val A1 = findViewById<TextView>(R.id.A1)
+        val B1 = findViewById<TextView>(R.id.B1)
+        val C1 = findViewById<TextView>(R.id.C1)
+        val D1 = findViewById<TextView>(R.id.D1)
+        val A2 = findViewById<TextView>(R.id.A2)
+        val B2 = findViewById<TextView>(R.id.B2)
+        val C2 = findViewById<TextView>(R.id.C2)
+        val D2 = findViewById<TextView>(R.id.D2)
+        val A3 = findViewById<TextView>(R.id.A3)
+        val B3 = findViewById<TextView>(R.id.B3)
+        val C3 = findViewById<TextView>(R.id.C3)
+        val D3 = findViewById<TextView>(R.id.D3)
+        val A4 = findViewById<TextView>(R.id.A4)
+        val B4 = findViewById<TextView>(R.id.B4)
+        val C4 = findViewById<TextView>(R.id.C4)
+        val D4 = findViewById<TextView>(R.id.D4)
+
+
+        if(currentDifficulty == "easy") {
+
+        }
+        if(currentDifficulty == "difficult") {
+
+        }
     }
 }
