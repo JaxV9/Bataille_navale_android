529413ba9f411d62c98b8cb5d18476044a84c576
JaxV9 <layanj.pro@gmail.com>
2024-06-07 12:37:04 +0200

    feat: player two could play

--- app/src/main/java/com/example/bataille_navale/PlayerOne.kt
+++ app/src/main/java/com/example/bataille_navale/PlayerOne.kt
@@ -18,53 +18,53 @@ class PlayerOne : AppCompatActivity() {
         findViewById<TextView>(R.id.playerOneInstructions).text = currentInstruction
 
         val A1 = findViewById<Button>(R.id.A1)
-        A1.setOnClickListener { placeShip(A1) }
+        A1.setOnClickListener { placeShip(A1, currentDifficulty.toString()) }
         val B1 = findViewById<Button>(R.id.B1)
-        B1.setOnClickListener { placeShip(B1) }
+        B1.setOnClickListener { placeShip(B1, currentDifficulty.toString()) }
         val C1 = findViewById<Button>(R.id.C1)
-        C1.setOnClickListener { placeShip(C1) }
+        C1.setOnClickListener { placeShip(C1, currentDifficulty.toString()) }
         val D1 = findViewById<Button>(R.id.D1)
-        D1.setOnClickListener { placeShip(D1) }
+        D1.setOnClickListener { placeShip(D1, currentDifficulty.toString()) }
         val A2 = findViewById<Button>(R.id.A2)
-        A2.setOnClickListener { placeShip(A2) }
+        A2.setOnClickListener { placeShip(A2, currentDifficulty.toString()) }
         val B2 = findViewById<Button>(R.id.B2)
-        B2.setOnClickListener { placeShip(B2) }
+        B2.setOnClickListener { placeShip(B2, currentDifficulty.toString()) }
         val C2 = findViewById<Button>(R.id.C2)
-        C2.setOnClickListener { placeShip(C2) }
+        C2.setOnClickListener { placeShip(C2, currentDifficulty.toString()) }
         val D2 = findViewById<Button>(R.id.D2)
-        D2.setOnClickListener { placeShip(D2) }
+        D2.setOnClickListener { placeShip(D2, currentDifficulty.toString()) }
         val A3 = findViewById<Button>(R.id.A3)
-        A3.setOnClickListener { placeShip(A3) }
+        A3.setOnClickListener { placeShip(A3, currentDifficulty.toString()) }
         val B3 = findViewById<Button>(R.id.B3)
-        B3.setOnClickListener { placeShip(B3) }
+        B3.setOnClickListener { placeShip(B3, currentDifficulty.toString()) }
         val C3 = findViewById<Button>(R.id.C3)
-        C3.setOnClickListener { placeShip(C3) }
+        C3.setOnClickListener { placeShip(C3, currentDifficulty.toString()) }
         val D3 = findViewById<Button>(R.id.D3)
-        D3.setOnClickListener { placeShip(D3) }
+        D3.setOnClickListener { placeShip(D3, currentDifficulty.toString()) }
         val A4 = findViewById<Button>(R.id.A4)
-        A4.setOnClickListener { placeShip(A4) }
+        A4.setOnClickListener { placeShip(A4, currentDifficulty.toString()) }
         val B4 = findViewById<Button>(R.id.B4)
-        B4.setOnClickListener { placeShip(B4) }
+        B4.setOnClickListener { placeShip(B4, currentDifficulty.toString()) }
         val C4 = findViewById<Button>(R.id.C4)
-        C4.setOnClickListener { placeShip(C4) }
+        C4.setOnClickListener { placeShip(C4, currentDifficulty.toString()) }
         val D4 = findViewById<Button>(R.id.D4)
-        D4.setOnClickListener { placeShip(D4) }
+        D4.setOnClickListener { placeShip(D4, currentDifficulty.toString()) }
         val A5 = findViewById<Button>(R.id.A5)
-        A5.setOnClickListener { placeShip(A5) }
+        A5.setOnClickListener { placeShip(A5, currentDifficulty.toString()) }
         val B5 = findViewById<Button>(R.id.B5)
-        B5.setOnClickListener { placeShip(B5) }
+        B5.setOnClickListener { placeShip(B5, currentDifficulty.toString()) }
         val C5 = findViewById<Button>(R.id.C5)
-        C5.setOnClickListener { placeShip(C5) }
+        C5.setOnClickListener { placeShip(C5, currentDifficulty.toString()) }
         val D5 = findViewById<Button>(R.id.D5)
-        D5.setOnClickListener { placeShip(D5) }
+        D5.setOnClickListener { placeShip(D5, currentDifficulty.toString()) }
         val A6 = findViewById<Button>(R.id.A6)
-        A6.setOnClickListener { placeShip(A6) }
+        A6.setOnClickListener { placeShip(A6, currentDifficulty.toString()) }
         val B6 = findViewById<Button>(R.id.B6)
-        B6.setOnClickListener { placeShip(B6) }
+        B6.setOnClickListener { placeShip(B6, currentDifficulty.toString()) }
         val C6 = findViewById<Button>(R.id.C6)
-        C6.setOnClickListener { placeShip(C6) }
+        C6.setOnClickListener { placeShip(C6, currentDifficulty.toString()) }
         val D6 = findViewById<Button>(R.id.D6)
-        D6.setOnClickListener { placeShip(D6) }
+        D6.setOnClickListener { placeShip(D6, currentDifficulty.toString()) }
 
         if(currentDifficulty == "easy") {
             A5.visibility = View.INVISIBLE
@@ -78,9 +78,9 @@ class PlayerOne : AppCompatActivity() {
         }
     }
 
-    private fun placeShip(button: Button) {
+    private fun placeShip(button: Button, currentDifficulty: String) {
             val intent = Intent(this, PlayerTwo::class.java)
-            intent.putExtra("DIFFICULTY", "easy")
+            intent.putExtra("DIFFICULTY", currentDifficulty)
             intent.putExtra("INSTRUCTIONS", "Player 2, try to find the ship.")
             intent.putExtra("PLAYERONESHIP", button.text.toString())
             startActivity(intent)
--- app/src/main/java/com/example/bataille_navale/PlayerTwo.kt
+++ app/src/main/java/com/example/bataille_navale/PlayerTwo.kt
@@ -2,6 +2,8 @@ package com.example.bataille_navale
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import org.w3c.dom.Text
@@ -14,11 +16,109 @@ class PlayerTwo : AppCompatActivity() {
         val currentDifficulty = intent.extras?.getString("DIFFICULTY")
         val currentInstruction = intent.extras?.getString("INSTRUCTIONS")
         val playerOneShip = intent.extras?.getString("PLAYERONESHIP")
+
         findViewById<TextView>(R.id.currentDifficulty).text = currentDifficulty
         findViewById<TextView>(R.id.playerTwoInstructions).text = currentInstruction
+
+        val A1 = findViewById<Button>(R.id.A1)
+        A1.setOnClickListener { findShip(A1) }
+        val B1 = findViewById<Button>(R.id.B1)
+        B1.setOnClickListener { findShip(B1) }
+        val C1 = findViewById<Button>(R.id.C1)
+        C1.setOnClickListener { findShip(C1) }
+        val D1 = findViewById<Button>(R.id.D1)
+        D1.setOnClickListener { findShip(D1) }
+        val A2 = findViewById<Button>(R.id.A2)
+        A2.setOnClickListener { findShip(A2) }
+        val B2 = findViewById<Button>(R.id.B2)
+        B2.setOnClickListener { findShip(B2) }
+        val C2 = findViewById<Button>(R.id.C2)
+        C2.setOnClickListener { findShip(C2) }
+        val D2 = findViewById<Button>(R.id.D2)
+        D2.setOnClickListener { findShip(D2) }
+        val A3 = findViewById<Button>(R.id.A3)
+        A3.setOnClickListener { findShip(A3) }
+        val B3 = findViewById<Button>(R.id.B3)
+        B3.setOnClickListener { findShip(B3) }
+        val C3 = findViewById<Button>(R.id.C3)
+        C3.setOnClickListener { findShip(C3) }
+        val D3 = findViewById<Button>(R.id.D3)
+        D3.setOnClickListener { findShip(D3) }
+        val A4 = findViewById<Button>(R.id.A4)
+        A4.setOnClickListener { findShip(A4) }
+        val B4 = findViewById<Button>(R.id.B4)
+        B4.setOnClickListener { findShip(B4) }
+        val C4 = findViewById<Button>(R.id.C4)
+        C4.setOnClickListener { findShip(C4) }
+        val D4 = findViewById<Button>(R.id.D4)
+        D4.setOnClickListener { findShip(D4) }
+        val A5 = findViewById<Button>(R.id.A5)
+        A5.setOnClickListener { findShip(A5) }
+        val B5 = findViewById<Button>(R.id.B5)
+        B5.setOnClickListener { findShip(B5) }
+        val C5 = findViewById<Button>(R.id.C5)
+        C5.setOnClickListener { findShip(C5) }
+        val D5 = findViewById<Button>(R.id.D5)
+        D5.setOnClickListener { findShip(D5) }
+        val A6 = findViewById<Button>(R.id.A6)
+        A6.setOnClickListener { findShip(A6) }
+        val B6 = findViewById<Button>(R.id.B6)
+        B6.setOnClickListener { findShip(B6) }
+        val C6 = findViewById<Button>(R.id.C6)
+        C6.setOnClickListener { findShip(C6) }
+        val D6 = findViewById<Button>(R.id.D6)
+        D6.setOnClickListener { findShip(D6) }
+
+        if(currentDifficulty == "easy") {
+            A5.visibility = View.INVISIBLE
+            B5.visibility = View.INVISIBLE
+            C5.visibility = View.INVISIBLE
+            D5.visibility = View.INVISIBLE
+            A6.visibility = View.INVISIBLE
+            B6.visibility = View.INVISIBLE
+            C6.visibility = View.INVISIBLE
+            D6.visibility = View.INVISIBLE
+        }
     }
 
+    private fun convertLetterToNumber(letter: String): Int {
+        return when (letter) {
+            "A" -> 1
+            "B" -> 2
+            "C" -> 3
+            "D" -> 4
+            else -> 0
+        }
+    }
     private fun findShip(button: Button){
 
+
+        val playerOnePos = intent.extras?.getString("PLAYERONESHIP")
+        val playerOnePosArray = arrayOf(convertLetterToNumber(playerOnePos!![0].toString()), playerOnePos.substring(1).toInt())
+
+        val thisBtn = button.text.toString()
+        val playerTwoSearchingArray = arrayOf(convertLetterToNumber(thisBtn[0].toString()), thisBtn.substring(1).toInt())
+
+        val calcCloseLetter = (playerOnePosArray[0] - playerTwoSearchingArray[0])
+        val calcCloseNumber = (playerOnePosArray[1] - playerTwoSearchingArray[1])
+        val playerTwoCloseToShip = calcCloseLetter + calcCloseNumber
+
+        val thisPlayerTwoCloseToShip = kotlin.math.abs(playerTwoCloseToShip)
+
+        if(thisPlayerTwoCloseToShip == 0){
+            findViewById<TextView>(R.id.playerTwoResult).text = "C'est gagné !"
+        }
+        if(thisPlayerTwoCloseToShip == 1) {
+            findViewById<TextView>(R.id.playerTwoResult).text = "Tu es proche"
+        }
+        if(thisPlayerTwoCloseToShip == 2) {
+            findViewById<TextView>(R.id.playerTwoResult).text = "Pas encore"
+        }
+        if(thisPlayerTwoCloseToShip == 3) {
+            findViewById<TextView>(R.id.playerTwoResult).text = "Tu es loin"
+        }
+        if(thisPlayerTwoCloseToShip >= 4) {
+            findViewById<TextView>(R.id.playerTwoResult).text = "Tu es à côté de la plaque !"
+        }
     }
 }
