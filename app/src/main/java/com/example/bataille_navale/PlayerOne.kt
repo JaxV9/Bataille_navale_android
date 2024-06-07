71f48ff85449dee6e32c140fde4a5a2d509e79be
JaxV9 <layanj.pro@gmail.com>
2024-06-07 10:46:16 +0200

    feat: difficulty added

--- app/src/main/java/com/example/bataille_navale/PlayerOne.kt
+++ app/src/main/java/com/example/bataille_navale/PlayerOne.kt
@@ -2,6 +2,7 @@ package com.example.bataille_navale
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class PlayerOne : AppCompatActivity() {
@@ -28,10 +29,25 @@ class PlayerOne : AppCompatActivity() {
         val B4 = findViewById<TextView>(R.id.B4)
         val C4 = findViewById<TextView>(R.id.C4)
         val D4 = findViewById<TextView>(R.id.D4)
+        val A5 = findViewById<TextView>(R.id.A5)
+        val B5 = findViewById<TextView>(R.id.B5)
+        val C5 = findViewById<TextView>(R.id.C5)
+        val D5 = findViewById<TextView>(R.id.D5)
+        val A6 = findViewById<TextView>(R.id.A6)
+        val B6 = findViewById<TextView>(R.id.B6)
+        val C6 = findViewById<TextView>(R.id.C6)
+        val D6 = findViewById<TextView>(R.id.D6)
 
 
         if(currentDifficulty == "easy") {
-
+            A5.setVisibility(View.INVISIBLE)
+            B5.setVisibility(View.INVISIBLE)
+            C5.setVisibility(View.INVISIBLE)
+            D5.setVisibility(View.INVISIBLE)
+            A6.setVisibility(View.INVISIBLE)
+            B6.setVisibility(View.INVISIBLE)
+            C6.setVisibility(View.INVISIBLE)
+            D6.setVisibility(View.INVISIBLE)
         }
         if(currentDifficulty == "difficult") {
 
